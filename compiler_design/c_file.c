0855de0c11a58ec59a760822e6bb7693b0dcb5b2
belal-bh <bh.pro.pust@gmail.com>
2018-08-04 01:57:08 +0600

    now this is valid for c_compiler.c program

--- compiler_design/c_file.c
+++ compiler_design/c_file.c
@@ -1,9 +1,22 @@
-#       include   <stdio.h>
+# include <stdio.h>
 
-int main(){
-    int x,s,y;
-    float ksd747,jfdjfue;
-    int kd;
+#define PI 3.1416
 
-    return 0 ;
+#  define I am Belal
+
+int foo(){
+    int a,b;
+    a=10 ;
+    b=5 ;
+    return 15;
+}
+
+int main()
+{
+ int x,s,y;
+ x=90;
+ y=x; s=x + 37/23;
+ float ks_d747_,jfdjfue_;
+ foo( );
+ return 0;
 }
